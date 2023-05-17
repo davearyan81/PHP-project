419d11c016fdb226d487c7140af409b67bd65f8e
davearyan81 <83450829+davearyan81@users.noreply.github.com>
2023-05-17 23:03:44 +0530

    Harnil changed all things

--- contactus.php
+++ contactus.php
@@ -3,9 +3,9 @@ include("includes/header.php");
 include("includes/navbar.php");
 if (isset($_POST['btnsubmit'])) {
 
-    $to = "harnilkadia@gmail.com";
-    $sub = "Contact Us";
-    $body = '<!DOCTYPE html>
+  $to = "salesforcearyan@gmail.com";
+  $sub = "Contact Us";
+  $body = '<!DOCTYPE html>
     <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
     
     <head>
@@ -162,140 +162,137 @@ if (isset($_POST['btnsubmit'])) {
     </body>
     
     </html>';
-    $header = array(
-        'From' => 'harnilkadia99@gmail.com',
-        'MIME-Versio' => '1.0',
-        'Content-type' => 'text/html; charset=iso-8859-1'
-    );
-    if (mail($to, $sub, $body, $header)) {
-        echo "
+  $header = array(
+    'From' => 'davearyan81@gmail.com',
+    'MIME-Versio' => '1.0',
+    'Content-type' => 'text/html; charset=iso-8859-1'
+  );
+  if (mail($to, $sub, $body, $header)) {
+    echo "
         <script>
         alert('Mail sent successfully.......');
         </script>
         ";
-    }
+  }
 }
 ?>
 
 
 <section class="ftco-section ">
-    <div class="container mx-">
-        <div class="row justify-content-center">
-            <div class="col-md-6 text-center mb-5">
-                <h1 style="color:red; text-decoration: underline;">Contact Us</h1>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="wrapper">
-                    <div class="row no-gutters mb-5">
-                        <div class="col-md-6">
-                            <div class="contact-wrap w-100 p-md-5 p-4">
-                                <h3 class="mb-4">Contact Us</h3>
-                                <div id="form-message-warning" class="mb-4"></div>
-                                <div id="form-message-success" class="mb-4">
-                                    Your message was sent, thank you!
-                                </div>
-                                <form method="POST" id="contactForm" name="contactForm" class="contactForm">
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <div class="form-group">
-                                                <label class="label" for="name">Full Name</label>
-                                                <input type="text" class="form-control" name="txtname" id="name"
-                                                    placeholder="Name" required>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="form-group">
-                                                <label class="label" for="email">Email Address</label>
-                                                <input type="email" class="form-control" name="txtemail" id="email"
-                                                    placeholder="Email" required>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label class="label" for="mobile">Mobile No</label>
-                                                <input type="text" class="form-control" name="txtmobile" id="mobile"
-                                                    placeholder="mobile" required>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label class="label" for="#">Message</label>
-                                                <textarea class="form-control" name="txtmessage" id="message" cols="30"
-                                                    rows="4" placeholder="Message"></textarea>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <button class="btn btn-danger" name="btnsubmit"
-                                                    value="submit">Submit</button>
-                                                <div class="submitting"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="col-md-5 d-flex align-items-stretch">
-                            <div id="map">
-                                <iframe
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.3467468217195!2d72.53128717443255!3d23.04774591538998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e84a538c7ede3%3A0xb31c13f9dbf13b9d!2sRaju%20Complex%2C%20Gurukul%20Rd%2C%20Memnagar%2C%20Ahmedabad%2C%20Gujarat%20380052!5e0!3m2!1sen!2sin!4v1682302051075!5m2!1sen!2sin"
-                                    width="600" height="570" style="border:0;" allowfullscreen="" loading="lazy"
-                                    referrerpolicy="no-referrer-when-downgrade"></iframe>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div class="dbox w-100 text-center">
-                                <div class="icon d-flex align-items-center justify-content-center">
-                                    <span class="fa fa-map-marker"></span>
-                                </div>
-                                <div class="text">
-                                    <p><span>Address:</span> Raju Complex
-                                        Gurukul Rd, Memnagar, Ahmedabad, Gujarat 380052
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-3">
-                            <div class="dbox w-100 text-center">
-                                <div class="icon d-flex align-items-center justify-content-center">
-                                    <span class="fa fa-phone"></span>
-                                </div>
-                                <div class="text">
-                                    <p><span>Phone:</span> <a href="tel://1234567920">+91 7069285585</a></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-3">
-                            <div class="dbox w-100 text-center">
-                                <div class="icon d-flex align-items-center justify-content-center">
-                                    <span class="fa fa-paper-plane"></span>
-                                </div>
-                                <div class="text">
-                                    <p><span>Email:</span> <a
-                                            href="mailto:salesforcearyan@gmail.com">pooja.com@gmail.com</a>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-3">
-                            <div class="dbox w-100 text-center">
-                                <div class="icon d-flex align-items-center justify-content-center">
-                                    <span class="fa fa-globe"></span>
-                                </div>
-                                <div class="text">
-                                    <p><span>Website</span> <a href="">pooja.com</a></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="container mx-">
+    <div class="row justify-content-center">
+      <div class="col-md-6 text-center mb-5">
+        <h1 style="color:red; text-decoration: underline;">Contact Us</h1>
+      </div>
     </div>
+    <div class="row justify-content-center">
+      <div class="col-md-12">
+        <div class="wrapper">
+          <div class="row no-gutters mb-5">
+            <div class="col-md-6">
+              <div class="contact-wrap w-100 p-md-5 p-4">
+                <h3 class="mb-4">Contact Us</h3>
+                <div id="form-message-warning" class="mb-4"></div>
+                <div id="form-message-success" class="mb-4">
+                  Your message was sent, thank you!
+                </div>
+                <form method="POST" id="contactForm" name="contactForm" class="contactForm">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label class="label" for="name">Full Name</label>
+                        <input type="text" class="form-control" name="txtname" id="name" placeholder="Name" required>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label class="label" for="email">Email Address</label>
+                        <input type="email" class="form-control" name="txtemail" id="email" placeholder="Email"
+                          required>
+                      </div>
+                    </div>
+                    <div class="col-md-12">
+                      <div class="form-group">
+                        <label class="label" for="mobile">Mobile No</label>
+                        <input type="text" class="form-control" name="txtmobile" id="mobile" placeholder="mobile"
+                          required>
+                      </div>
+                    </div>
+                    <div class="col-md-12">
+                      <div class="form-group">
+                        <label class="label" for="#">Message</label>
+                        <textarea class="form-control" name="txtmessage" id="message" cols="30" rows="4"
+                          placeholder="Message"></textarea>
+                      </div>
+                    </div>
+                    <div class="col-md-12">
+                      <div class="form-group">
+                        <button class="btn btn-danger" name="btnsubmit" value="submit">Submit</button>
+                        <div class="submitting"></div>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="col-md-5 d-flex align-items-stretch">
+              <div id="map">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.3467468217195!2d72.53128717443255!3d23.04774591538998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e84a538c7ede3%3A0xb31c13f9dbf13b9d!2sRaju%20Complex%2C%20Gurukul%20Rd%2C%20Memnagar%2C%20Ahmedabad%2C%20Gujarat%20380052!5e0!3m2!1sen!2sin!4v1682302051075!5m2!1sen!2sin"
+                  width="600" height="570" style="border:0;" allowfullscreen="" loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade"></iframe>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-3">
+              <div class="dbox w-100 text-center">
+                <div class="icon d-flex align-items-center justify-content-center">
+                  <span class="fa fa-map-marker"></span>
+                </div>
+                <div class="text">
+                  <p><span>Address:</span> Raju Complex
+                    Gurukul Rd, Memnagar, Ahmedabad, Gujarat 380052
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="dbox w-100 text-center">
+                <div class="icon d-flex align-items-center justify-content-center">
+                  <span class="fa fa-phone"></span>
+                </div>
+                <div class="text">
+                  <p><span>Phone:</span> <a href="tel://1234567920">+91 7069285585</a></p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="dbox w-100 text-center">
+                <div class="icon d-flex align-items-center justify-content-center">
+                  <span class="fa fa-paper-plane"></span>
+                </div>
+                <div class="text">
+                  <p><span>Email:</span> <a href="mailto:salesforcearyan@gmail.com">pooja.com@gmail.com</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="dbox w-100 text-center">
+                <div class="icon d-flex align-items-center justify-content-center">
+                  <span class="fa fa-globe"></span>
+                </div>
+                <div class="text">
+                  <p><span>Website</span> <a href="">pooja.com</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 <?php include("includes/scripts.php"); ?>
 
--- profile.php
+++ profile.php
@@ -7,8 +7,7 @@ if (isset($_SESSION['auth'])) {
     $qry = "select * from user where uid='$id'";
     $result = $con->query($qry);
     $row = $result->fetch_assoc();
-}
-else{
+} else {
     header("location:index.php");
 }
 ?>
@@ -113,40 +112,18 @@ else{
                     </div>
                     <div class="card mt-3">
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round"
-                                        class="feather feather-globe mr-2 icon-inline">
-                                        <circle cx="12" cy="12" r="10"></circle>
-                                        <line x1="2" y1="12" x2="22" y2="12"></line>
-                                        <path
-                                            d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z">
-                                        </path>
-                                    </svg>Website</h6>
-                                <span class="text-secondary">https://bootdey.com</span>
-                            </li>
+
                             <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                                 <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                         stroke-linecap="round" stroke-linejoin="round"
                                         class="feather feather-github mr-2 icon-inline">
+
                                         <path
                                             d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
                                         </path>
-                                    </svg>Github</h6>
-                                <span class="text-secondary">bootdey</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round"
-                                        class="feather feather-twitter mr-2 icon-inline text-info">
-                                        <path
-                                            d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z">
-                                        </path>
-                                    </svg>Twitter</h6>
-                                <span class="text-secondary">@bootdey</span>
+                                    </svg><a href=" https://github.com/" style="color:black;"> Github </a></h6>
+                                <span class=" text-secondary"></span>
                             </li>
                             <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                                 <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
@@ -156,8 +133,8 @@ else{
                                         <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
                                         <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
                                         <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-                                    </svg>Instagram</h6>
-                                <span class="text-secondary">bootdey</span>
+                                    </svg><a href=" https://github.com/" class="text-danger"> Instagram </a></h6>
+                                <span class="text-secondary"></span>
                             </li>
                             <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                                 <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
@@ -166,8 +143,8 @@ else{
                                         class="feather feather-facebook mr-2 icon-inline text-primary">
                                         <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z">
                                         </path>
-                                    </svg>Facebook</h6>
-                                <span class="text-secondary">bootdey</span>
+                                    </svg><a href=" https://facebook.com/"> Facebook </a></h6>
+                                <span class="text-secondary"><a href="https://www.facebook.com"></a></span>
                             </li>
                         </ul>
                     </div>
@@ -190,7 +167,8 @@ else{
                                         <h6 class="mb-0">Email</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" name="txtemail" value="<?= $row['email'] ?>">
+                                        <input type="text" class="form-control" name="txtemail"
+                                            value="<?= $row['email'] ?>">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
@@ -198,7 +176,8 @@ else{
                                         <h6 class="mb-0">Phone</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" name="txtphnum" value="<?= $row['phnum'] ?>">
+                                        <input type="text" class="form-control" name="txtphnum"
+                                            value="<?= $row['phnum'] ?>">
                                     </div>
                                 </div>
 
@@ -207,13 +186,15 @@ else{
                                         <h6 class="mb-0">Address</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" name="txtaddress" value="<?= $row['address'] ?>">
+                                        <input type="text" class="form-control" name="txtaddress"
+                                            value="<?= $row['address'] ?>">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-3"></div>
                                     <div class="col-sm-9 text-secondary">
-                                        <button type="submit" class="btn btn-danger px-4" name="submit">Save Change</button>
+                                        <button type="submit" class="btn btn-danger px-4" name="submit">Save
+                                            Change</button>
                                     </div>
                                 </div>
                             </form>
@@ -236,14 +217,13 @@ else{
 </html>
 <?php
 if (isset($_POST["submit"])) {
-    $name = explode(" ",$_POST['txtname']);
-    $email=$_POST['txtemail'];
-    $ph=$_POST['txtphnum'];
-    $address=$_POST['txtaddress'];
-    $qry1="UPDATE user SET fname='$name[0]',lname='$name[1]',email='$email',phnum='$ph',address='$address' WHERE uid='$id'";
-    $result1=$con->query($qry1);
-    if($result1)
-    {
+    $name = explode(" ", $_POST['txtname']);
+    $email = $_POST['txtemail'];
+    $ph = $_POST['txtphnum'];
+    $address = $_POST['txtaddress'];
+    $qry1 = "UPDATE user SET fname='$name[0]',lname='$name[1]',email='$email',phnum='$ph',address='$address' WHERE uid='$id'";
+    $result1 = $con->query($qry1);
+    if ($result1) {
         echo "<script>window.location.href='profile.php'</script>";
     }
     // print_r($name[0]);
